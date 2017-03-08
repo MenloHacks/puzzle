b4a05b395458d2be067c436d29d122de4b3e0e28
Thomas Woodside <thomas.woodside@gmail.com>
2017-03-08 15:19:41 -0800

    static files

--- puzzle/settings.py
+++ puzzle/settings.py
@@ -107,6 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
