d397a6492ad93a8ff55199e93935560273588f17
Thomas Woodside <thomas.woodside@gmail.com>
2017-03-08 16:27:02 -0800

    heroku settings

--- puzzle/settings.py
+++ puzzle/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'puzzle.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'puzzle/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
