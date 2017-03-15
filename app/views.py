13ddd0290f6f867714935d0ba61a24805b61363f
thomas <thomas.woodside@gmail.com>
2017-03-14 17:23:53 -0700

    Added a page for before the puzzle opens.

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect
 from django.views.generic import View
 from django.contrib.sessions.models import Session
 from django.http import HttpResponse
+from datetime import datetime
 
+start_time = datetime(2017, 3, 18, 1, 0, 0, 0)
 def intro(request):
+    if datetime.now() < start_time:
+        return render(request, "before_opening.html")
     return render(request, "intro.html")
 
 def clue_1_5(request):
